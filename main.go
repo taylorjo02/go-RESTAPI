1d61b55cdb63fc2a695ffcfa44e7555d7b2ea224
taylorjo02 <joshua.d.taylor122@gmail.com>
2025-03-08 23:41:05 -0500

    feat: add create route, api test files, and save function to save events on a POST request

--- main.go
+++ main.go
@@ -5,18 +5,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/taylorjo02/go-RESTAPI/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) 
+	server.POST("events", createEvents)
 
-	err := server.Run(":8080"); if err != nil{
+	err := server.Run(":8080"); if err != nil {
 		log.Fatal("unable to start server, %w", err)
 	} 
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvents(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event); if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
